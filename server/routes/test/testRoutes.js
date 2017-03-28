7f99e6b0960484243c5a4c6f11570ea33d632db9
Jaylan <Jaylan.Tse@gmail.com>
2017-03-27 23:28:16 -0700

    Example code for accessing database and returning result

--- server/routes/test/testRoutes.js
+++ server/routes/test/testRoutes.js
@@ -1,22 +1,34 @@
 const router = require('express').Router();
-const ibmdb = require('ibm_db');
+const Promise = require('bluebird');
+const ibmdb = Promise.promisifyAll(require('ibm_db'));
+//const ibmdb = require('ibm_db');
 const config = require('../config');
 
 router.route('/')
 .get((req, res, next) => {
-	ibmdb.open(config, (err, conn) => {
-		if (err) return console.log(err);
+//  Callbacks
+//	ibmdb.open(config, (err, conn) => {
+//		if (err) return res.status(500).send(err.message);
+//
+//		conn.query('select * from test', (err, rows) => {
+//			if (err) return res.status(400).send(err.message);
+//
+//			conn.close();
+//			res.send(rows);
+//		});
+//	});
 
-		conn.query('select * from myTest', (err, data) => {
-			if (err) console.log(err);
-			else console.log(data);
-
-			conn.close(() => {
-				console.log('done');
-			});
-			res.json(data);
-		});
-	});
+//  Promise
+	ibmdb.open(config)
+		.catch(err => res.status(500).send(err.message))
+		.then(conn => { 
+			return conn.query('select * from test')
+						.then(rows => {
+							conn.close();
+							res.send(rows);
+						});
+		})
+		.catch(err => res.status(400).send(err.message));
 });
 
 module.exports = router;
