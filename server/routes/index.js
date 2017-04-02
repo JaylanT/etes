11c572530a9cf00325e00f032defaaa5dd8adcb1
Jaylan <Jaylan.Tse@gmail.com>
2017-04-02 16:37:46 -0700

    Remove auth check for get on /tickets

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,12 @@ router.get('/', (req, res) => {
 	res.send("Hello from ETES API");
 });
 
+router.use('/tickets', require('./tickets'));
+
 const authCheckMiddleware = require('../middleware/auth-check');
 router.use('/tickets', authCheckMiddleware);
-router.use('/tickets', require('./tickets'));
+router.use('/tickets', require('./tickets-auth'));
+
 router.use('/auth', require('./auth'));
 
 router.use((err, req, res, next) => {
--- server/routes/tickets.js
+++ server/routes/tickets.js
@@ -1,44 +1,8 @@
 const router = require('express').Router();
 const ibmdb = require('../modules/ibmdb');
-const jwt = require('jsonwebtoken');
-const jwtConfig = require('../config/jwt-config');
 
 
 router.route('/')
-	.post((req, res, next) => {
-		const data = req.body;
-		const validation = validateTicket(data);
-
-		if (!validation.success) {
-			delete validation.success;
-			return res.status(400).json(validation);
-		}
-
-		// jwt should have been previously verified
-		const token = req.headers.authorization.split(' ')[1];
-		const decoded = jwt.verify(token, jwtConfig.secret);
-		let sellerId;
-		try {
-			sellerId = decoded.sub;
-		} catch(err) {
-			return res.status(401).end();
-		}
-
-		const sql = 'INSERT INTO TICKETS (SELLER_ID, LISTING_TITLE, DESCRIPTION, PRICE, CREATED_AT) ' +
-						'VALUES (?, ?, ?, ?, CURRENT TIMESTAMP)';
-
-		ibmdb.executeNonQuery(sql, [sellerId, data.listingTitle, data.description, data.price])
-			.then(ret => {
-				if (ret !== 1) {
-					throw Error('Insert failed.');
-				}
-				res.send('Ticket inserted.');
-			})
-			.catch(err => res.status(400).send({
-				status: 400,
-				message: err.message
-			}));
-	})
 	.get((req, res, next) => {
 		const limit = req.query.limit || 50,
 				start = req.query.start || 0,
@@ -76,32 +40,6 @@ router.route('/:id')
 			}));
 	});
 
-function validateTicket(payload) {
-	let isFormValid = true;
-	const errors = {};
-
-	if (!payload || typeof payload.listingTitle !== 'string' || payload.listingTitle.trim().length === 0) {
-		isFormValid = false;
-		errors.listingTitle = 'Please enter a listing title.';
-	}
-	if (!payload || typeof payload.description !== 'string' || payload.description.trim().length === 0) {
-		isFormValid = false;
-		errors.description = 'Please enter a description.';
-	}
-	if (!payload || isNaN(payload.price) || payload.price <= 0) {
-		isFormValid = false;
-		errors.price = 'Please enter a price above $0.00.';
-	}
-
-	const message = isFormValid ? '' : 'Form validation failed.';
-
-	return {
-		success: isFormValid,
-		message,
-		errors
-	};
-}
-
 function getOrderByIdentifier(orderBy) {
 	let identifier = '';
 
@@ -120,7 +58,6 @@ function getOrderByIdentifier(orderBy) {
 			break;
 		default:
 			identifier = 'CREATED_AT DESC';
-			break;
 	}
 	return identifier;
 }
