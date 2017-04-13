a5a1888c2c120fe339bee5cf75da9f6077611bf6
Thai Truong <akita.thai94@gmail.com>
2017-04-13 12:07:44 -0700

    Add Create Ticket
    Add Create Ticket Component

---
+++ src/components/Create.jsx
@@ -0,0 +1,20 @@
+import Auth from '../modules/Auth';
+import React, { Component } from 'react';
+
+
+class Create extends Component {
+	constructor(props) {
+		super(props);
+		this.create = this.create.bind(this);
+		this.state = {error: null};
+		}
+	createticket(e) {
+		e.preventDefault(); 
+		if(!Auth.isUserAuthenticated()) this.state = {error: true}
+		
+
+
+
+	}
+
+}
