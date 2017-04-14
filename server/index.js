5a62547799574c778c1bf8b8bdc34829b6563194
Jaylan <Jaylan.Tse@gmail.com>
2017-04-13 20:32:49 -0700

    Fixed pagination link generation.

--- server/index.js
+++ server/index.js
@@ -9,9 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Expose-Headers", "Link");
+	next();
 });
 
 app.use(passport.initialize());
