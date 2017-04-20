21a34c99ba2e3632dbac2893d149f68140a666ad
Jaylan <Jaylan.Tse@gmail.com>
2017-04-19 22:57:52 -0700

    Tickets can now be purchased.

--- server/routes/tickets-auth.js
+++ server/routes/tickets-auth.js
@@ -49,6 +49,7 @@ router.route('/')
 router.route('/:id/purchase')
 	.post((req, res, next) => {
 		const ticketId = req.params.id;
+		const data = req.body;
 
 		const token = req.headers.authorization.split(' ')[1];
 		let buyerId;
@@ -59,35 +60,42 @@ router.route('/:id/purchase')
 			return res.status(401).end();
 		}
 
-		const insertOrder = 'INSERT INTO ORDERS (TICKET_ID, BUYER_ID) VALUES (?, ?)';
-		const insertOrderParams = [ticketId, buyerId];
+		const insertOrder = 'INSERT INTO ORDERS (TICKET_ID, BUYER_ID, DATE_ORDERED, ' +
+			'SHIP_NAME, SHIP_ADDRESS_LINE_1, SHIP_ADDRESS_LINE_2, SHIP_CITY, SHIP_STATE, SHIP_ZIP) ' +
+			'VALUES (?, ?, CURRENT TIMESTAMP, ?, ?, ?, ?, ?, ?)';
+		const insertOrderParams = [ticketId, buyerId, data.name, data.addressLine1, data.addressLine2, data.city, data.state, data.zip];
 
 		const updateTicket = 'UPDATE TICKETS SET SOLD = 1 WHERE TICKET_ID = ?';
-		const updateTicketParams = [ticketId];
+
+		const findTicket = 'SELECT * FROM TICKETS WHERE TICKET_ID = ? AND SOLD = 0';
 
 		ibmdb.open().then(conn => {
-				return conn.beginTransactionAsync()
-					.then(() => ibmdb.prepareAndExecuteNonQuery(conn, insertOrder, insertOrderParams))
-					.then(ret => {
-						if (ret !== 1) {
-							throw Error('Purchase failed.');
-						}
-						return ibmdb.prepareAndExecuteNonQuery(conn, updateTicket, updateTicketParams);
-					})
-					.then(ret => {
-						if (ret !== 1) {
-							throw Error('Purchase failed.');
-						}
-						return conn.commitTransactionAsync();
-					})
-					.then(() => {
-						res.send({ message: 'ok' });
-						conn.closeSync();
-					})
-					.catch(err => {
-						conn.closeSync();
-						throw Error(err.message);
-					});
+			return ibmdb.prepareAndExecute(conn, findTicket, [ticketId])
+				.then(data => {
+					if (data.length === 0) throw Error('Ticket is unavailable for purchase.');
+					return conn.beginTransactionAsync();
+				})
+				.then(() => ibmdb.prepareAndExecuteNonQuery(conn, insertOrder, insertOrderParams))
+				.then(ret => {
+					if (ret !== 1) {
+						throw Error('Purchase failed.');
+					}
+					return ibmdb.prepareAndExecuteNonQuery(conn, updateTicket, [ticketId]);
+				})
+				.then(ret => {
+					if (ret !== 1) {
+						throw Error('Purchase failed.');
+					}
+					return conn.commitTransactionAsync();
+				})
+				.then(() => {
+					res.send({ message: 'Ticket #' + ticketId + ' purchased.' });
+					conn.closeSync();
+				})
+				.catch(err => {
+					conn.closeSync();
+					throw Error(err.message);
+				});
 			})
 			.catch(err => {
 				res.status(400).send({
@@ -97,35 +105,35 @@ router.route('/:id/purchase')
 			});
 	});
 
-		function validateTicket(payload) {
-			let isFormValid = true;
-			const errors = {};
+function validateTicket(payload) {
+	let isFormValid = true;
+	const errors = {};
 
-			if (!payload || typeof payload.title !== 'string' || payload.title.trim().length === 0) {
-				isFormValid = false;
-				errors.title = 'Please enter a listing title.';
-			}
-			if (!payload || typeof payload.description !== 'string' || payload.description.trim().length === 0) {
-				isFormValid = false;
-				errors.description = 'Please enter a description.';
-			}
-			if (!payload || isNaN(payload.price) || payload.price <= 0) {
-				isFormValid = false;
-				errors.price = 'Please enter a price above $0.00.';
-			}
-			const categories = ['Music', 'Sports', 'Arts & Theater', 'Family', 'Other'];
-			if (!payload || typeof payload.category !== 'string' || categories.indexOf(payload.category) < 0) {
-				isFormValid = false;
-				errors.category = 'Please select a category.';
-			}
+	if (!payload || typeof payload.title !== 'string' || payload.title.trim().length === 0) {
+		isFormValid = false;
+		errors.title = 'Please enter a listing title.';
+	}
+	if (!payload || typeof payload.description !== 'string' || payload.description.trim().length === 0) {
+		isFormValid = false;
+		errors.description = 'Please enter a description.';
+	}
+	if (!payload || isNaN(payload.price) || payload.price <= 0) {
+		isFormValid = false;
+		errors.price = 'Please enter a price above $0.00.';
+	}
+	const categories = ['Music', 'Sports', 'Arts & Theater', 'Family', 'Other'];
+	if (!payload || typeof payload.category !== 'string' || categories.indexOf(payload.category) < 0) {
+		isFormValid = false;
+		errors.category = 'Please select a category.';
+	}
 
-			const message = isFormValid ? '' : 'Form validation failed.';
+	const message = isFormValid ? '' : 'Form validation failed.';
 
-			return {
-				success: isFormValid,
-				message,
-				errors
-			};
-		}
+	return {
+		success: isFormValid,
+		message,
+		errors
+	};
+}
 
-		module.exports = router;
+module.exports = router;
