0965d4def9d54a65645cfadf0291f1e9d566267c
Jaylan <Jaylan.Tse@gmail.com>
2017-04-20 21:00:40 -0700

    Reorganized code. Added got library

--- server/routes/tickets-auth.js
+++ server/routes/tickets-auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const ibmdb = require('../modules/ibmdb');
 const jwt = require('jsonwebtoken');
 const jwtConfig = require('../config/jwt-config');
+const got = require('got');
 
 
 router.route('/')
@@ -60,37 +61,40 @@ router.route('/:id/purchase')
 			return res.status(401).end();
 		}
 
+		// find ticket that user intends to purchase and make sure it's not sold
+		const findTicket = 'SELECT * FROM TICKETS WHERE TICKET_ID = ? AND SOLD = 0';
+
 		const insertOrder = 'INSERT INTO ORDERS (TICKET_ID, BUYER_ID, DATE_ORDERED, ' +
 			'SHIP_NAME, SHIP_ADDRESS_LINE_1, SHIP_ADDRESS_LINE_2, SHIP_CITY, SHIP_STATE, SHIP_ZIP) ' +
 			'VALUES (?, ?, CURRENT TIMESTAMP, ?, ?, ?, ?, ?, ?)';
 		const insertOrderParams = [ticketId, buyerId, data.name, data.addressLine1, data.addressLine2, data.city, data.state, data.zip];
 
-		const updateTicket = 'UPDATE TICKETS SET SOLD = 1 WHERE TICKET_ID = ?';
-
-		const findTicket = 'SELECT * FROM TICKETS WHERE TICKET_ID = ? AND SOLD = 0';
+		// set ticket as sold
+		const updateTicket = 'UPDATE TICKETS SET SOLD = 1 WHERE TICKET_ID = ? AND SOLD = 0';
 
 		ibmdb.open().then(conn => {
 			return ibmdb.prepareAndExecute(conn, findTicket, [ticketId])
 				.then(data => {
 					if (data.length === 0) throw Error('Ticket is unavailable for purchase.');
-					return conn.beginTransactionAsync();
-				})
-				.then(() => ibmdb.prepareAndExecuteNonQuery(conn, insertOrder, insertOrderParams))
-				.then(ret => {
-					if (ret !== 1) {
-						throw Error('Purchase failed.');
-					}
-					return ibmdb.prepareAndExecuteNonQuery(conn, updateTicket, [ticketId]);
-				})
-				.then(ret => {
-					if (ret !== 1) {
-						throw Error('Purchase failed.');
-					}
-					return conn.commitTransactionAsync();
+					// use transaction to make sure order is inserted and ticket is set as sold
+					return conn.beginTransactionAsync()
+						.then(() => ibmdb.prepareAndExecuteNonQuery(conn, updateTicket, [ticketId]))
+						.then(ret => {
+							if (ret !== 1) throw Error('Purchase failed.');
+							return ibmdb.prepareAndExecuteNonQuery(conn, insertOrder, insertOrderParams);
+						})
+						.then(ret => {
+							if (ret !== 1) throw Error('Purchase failed.');
+						})
+						.catch(err => {
+							conn.rollbackTransactionSync();
+							throw Error(err.message);
+						})
+						.then(() => conn.commitTransactionAsync());
 				})
 				.then(() => {
-					res.send({ message: 'Ticket #' + ticketId + ' purchased.' });
-					conn.closeSync();
+					conn.close();
+					res.send({ message: 'Ticket purchased.' });
 				})
 				.catch(err => {
 					conn.closeSync();
