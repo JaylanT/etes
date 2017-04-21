a1dc7c70d317f83345c214ade9ee1d6ec8ba2ea3
Jaylan <Jaylan.Tse@gmail.com>
2017-04-21 15:46:55 -0700

    Added shipping time and distance calculation.

--- server/routes/tickets-auth.js
+++ server/routes/tickets-auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const ibmdb = require('../modules/ibmdb');
 const jwt = require('jsonwebtoken');
 const jwtConfig = require('../config/jwt-config');
+const googleConfig = require('../config/google-config');
 const got = require('got');
 
 
@@ -34,6 +35,10 @@ router.route('/')
 
 		ibmdb.open().then(conn => {
 			return ibmdb.prepareAndExecuteNonQuery(conn, sql, params)
+				.catch(err => {
+					conn.closeSync();
+					throw Error(err.message);
+				})
 				.then(ret => {
 					conn.close();
 					if (ret !== 1) {
@@ -63,13 +68,23 @@ router.route('/:id/purchase')
 		} catch(err) {
 			return res.status(401).end();
 		}
+		const buyerAddress = {
+			name: data.name,
+			addressLine1: data.addressLine1,
+			addressLine2: data.addressLine2,
+			city: data.city,
+			state: data.state,
+			zip: data.zip
+		};
 
-		// find ticket that user intends to purchase and make sure it's not sold
-		const findTicket = 'SELECT * FROM TICKETS WHERE TICKET_ID = ? AND SOLD = 0';
+		// find ticket and get address that user intends to purchase and make sure it's not sold
+		const findTicket = 'SELECT SELLER_NAME as "name", SELLER_ADDRESS_LINE_1 as "addressLine1", ' +
+			'SELLER_ADDRESS_LINE_2 as "addressLine2", SELLER_CITY as "city", SELLER_STATE as "state", ' +
+			'SELLER_ZIP as "zip" FROM TICKETS WHERE TICKET_ID = ? AND SOLD = 0';
 
 		const insertOrder = 'INSERT INTO ORDERS (TICKET_ID, BUYER_ID, DATE_ORDERED, ' +
-			'SHIP_NAME, SHIP_ADDRESS_LINE_1, SHIP_ADDRESS_LINE_2, SHIP_CITY, SHIP_STATE, SHIP_ZIP) ' +
-			'VALUES (?, ?, CURRENT TIMESTAMP, ?, ?, ?, ?, ?, ?)';
+			'SHIP_NAME, SHIP_ADDRESS_LINE_1, SHIP_ADDRESS_LINE_2, SHIP_CITY, SHIP_STATE, SHIP_ZIP, SHIP_DISTANCE, SHIP_TIME) ' +
+			'VALUES (?, ?, CURRENT TIMESTAMP, ?, ?, ?, ?, ?, ?, ?, ?)';
 		const insertOrderParams = [ticketId, buyerId, data.name, data.addressLine1, data.addressLine2, data.city, data.state, data.zip];
 
 		// set ticket as sold
@@ -79,12 +94,19 @@ router.route('/:id/purchase')
 			return ibmdb.prepareAndExecute(conn, findTicket, [ticketId])
 				.then(data => {
 					if (data.length === 0) throw Error('Ticket is unavailable for purchase.');
+					const sellerAddress = data[0];
+					return getShippingInfo(sellerAddress, buyerAddress);
+				})
+				.then(shippingInfo => {
+					if (shippingInfo.status !== 'OK') {
+						throw Error('Failed to calculate shipping information.');
+					}
 					// use transaction to make sure order is inserted and ticket is set as sold
 					return conn.beginTransactionAsync()
 						.then(() => ibmdb.prepareAndExecuteNonQuery(conn, updateTicket, [ticketId]))
 						.then(ret => {
 							if (ret !== 1) throw Error('Purchase failed.');
-							return ibmdb.prepareAndExecuteNonQuery(conn, insertOrder, insertOrderParams);
+							return ibmdb.prepareAndExecuteNonQuery(conn, insertOrder, [...insertOrderParams, shippingInfo.distance.text, shippingInfo.duration.text]);
 						})
 						.then(ret => {
 							if (ret !== 1) throw Error('Purchase failed.');
@@ -111,6 +133,16 @@ router.route('/:id/purchase')
 				});
 			});
 	});
+
+function getShippingInfo(sellerAddress, buyerAddress) {
+	const originAddress = `${sellerAddress.addressLine1},${sellerAddress.addressLine2},${sellerAddress.city},${sellerAddress.state},${sellerAddress.zip}`;
+	const destinationAddress = `${buyerAddress.addressLine1},${buyerAddress.addressLine2},${buyerAddress.city},${buyerAddress.state},${buyerAddress.zip}`;
+	return got(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${originAddress}&destinations=${destinationAddress}&key=${googleConfig.key}`)
+		.then(res => {
+			const body = JSON.parse(res.body);
+			return body.rows[0].elements[0];
+		});
+}
 
 function validateTicket(payload) {
 	let isFormValid = true;
